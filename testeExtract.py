f730c1d8ae19263836cc23d0f97333496346bfa9
Marcos <maasouza@poli.ufrj.br>
2016-10-19 20:19:58 -0200

    Extracting features from .wav files

---
+++ testeExtract.py
@@ -0,0 +1,83 @@
+from python_speech_features import mfcc
+from python_speech_features import logfbank
+import scipy.io.wavfile as wav
+
+(rate,sig) = wav.read("foo.wav")
+mfcc_feat = mfcc(sig,rate)
+fbank_feat = logfbank(sig,rate)
+
+for i in mfcc_feat:
+    print(i)
+print(len(mfcc_feat[0]) , len(mfcc_feat))
+"""
+def operation_cast(argument):
+    switcher = {
+        "mais"     : 0,
+        "menos"    : 1,
+        "dividido" : 2,
+        "vezes"    : 3,
+        "elevado"  : 4,
+    }
+    return switcher.get(argument, None)
+
+def number_cast(argument):
+    switcher = {
+        "zero"   : "0",
+        "um"     : "1",
+        "dois"   : "2",
+        "tres"   : "3",
+        "quatro" : "4",
+        "cinco"  : "5",
+        "seis"   : "6",
+        "sete"   : "7",
+        "oito"   : "8",
+        "nove"   : "9",
+    }
+
+    return switcher.get(argument, "")
+
+def data_extract(text):
+    retorna uma lista contendo (operação,x,y)
+
+    words= text.split(" ")
+
+    op = None
+    x = ""
+    y = ""
+
+    for word in words:
+        if (op == None):
+            if(word == "mais" or word == "menos" or word == "dividido" or word == "vezes" or word =="elevado"):
+                op = operation_cast(word)
+            else:
+                x+=number_cast(word)
+        else:
+            y += number_cast(word)
+
+    output = [op, int(x), int(y)]
+
+    return output
+
+def calculator(op, x, y):
+    if(x== None or y == None or op == None):
+        print("Numero ou operação invalida")
+        return None
+
+    if   (op == 0):
+        return (x + y)
+    elif (op == 1):
+        return (x - y)
+    elif (op == 2):
+        return (x / y)
+    elif (op == 3):
+        return (x * y)
+    elif (op == 4):
+        return (x**y)
+
+out = data_extract(s)
+
+res = calculator(out[0],out[1],out[2])
+
+print(res)
+
+"""
